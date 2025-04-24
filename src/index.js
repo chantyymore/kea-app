5c56a78b6386bd22d4062f4c07b5db890d1be249
Keamogetswe Moeketsi <167786154+chantyymore@users.noreply.github.com>
2025-04-24 12:33:22 +0200

    Added basic javascript to the search engine(no API)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-input");
+    //alert(searchInputElement.value);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= searchInputElement.value;
+    
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
