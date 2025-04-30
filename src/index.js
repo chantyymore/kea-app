81e84560de8f2e909fc7358c7c5fd667e8eb3cc9
Keamogetswe Moeketsi <167786154+chantyymore@users.noreply.github.com>
2025-04-30 13:12:50 +0200

    updated the search form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+function refreshWeather(response){
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#weather-app-temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city){
+    let apiKey = "6b534oeab5dabbea88bbtf6452c02346";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    console.log(apiURL);
+    axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-input");
-    //alert(searchInputElement.value);
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML= searchInputElement.value;
-    
+    searchCity(searchInputElement.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Midrand");
